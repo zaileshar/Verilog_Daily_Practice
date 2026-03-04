5760538a0cf0ab2192218a5c445ed2263624d53d
zaileshar <zaileshar@users.noreply.github.com>
2026-03-04 16:10:06 +0530

    improved subtractor 8bit

--- intermediate/subtractor_8bit/subtractor_8bit.v
+++ intermediate/subtractor_8bit/subtractor_8bit.v
@@ -1,3 +1,4 @@
+`default_nettype none
 `timescale 1ns/1ps
 module subtractor_8bit #(parameter WIDTH = 8) (
     input  [WIDTH-1:0] a, b,
@@ -6,3 +7,5 @@ module subtractor_8bit #(parameter WIDTH = 8) (
 );
     assign {borrow, diff} = a - b;
 endmodule
+
+`default_nettype wire
