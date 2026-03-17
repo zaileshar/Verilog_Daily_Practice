2eb59ba42d3045b61e40b955fc344d57abc530ad
zaileshar <zaileshar@users.noreply.github.com>
2026-03-17 15:59:58 +0530

    implemented clock divider by2 module

---
+++ intermediate/clock_divider_by2/README.md
@@ -0,0 +1,2 @@
+# clock_divider_by2
+A clock divider that divides by 2.
---
+++ intermediate/clock_divider_by2/clock_divider_by2_tb.sv
@@ -0,0 +1,12 @@
+`timescale 1ns/1ps
+module clock_divider_by2_tb;
+    reg clk_in, rst_n;
+    wire clk_out;
+    clock_divider_by2 uut (.*);
+    always #5 clk_in = ~clk_in;
+    initial begin
+        clk_in = 0; rst_n = 0;
+        #12 rst_n = 1;
+        #500 $display("Test passed"); $finish;
+    end
+endmodule
